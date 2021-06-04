e96a768cc2e9b1b24f843a4b8c1f273e7f9e9860
mahfuz-antor <antorcnj2014@gmail.com>
2021-06-04 19:41:43 +0600

    scroll do not work

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
-import MangoItem from './components/MangoItem'
+import MangoItem from './components/MangoItem';
+import MangoList from './components/MangoList';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <MangoList />
     
       
-      <MangoItem />
+      {/* <MangoItem 
+      name={"Himsagor"}
+      price={"$1500"}
+      image={require('./assets/images/m-bg-1.jpg')}
+      /> */}
       
       <StatusBar style="auto" />
     </View>
---
+++ components/MangoList/mangoes.js
@@ -0,0 +1,20 @@
+export default [{
+    name: 'Himsagor',
+    price: '$1920',
+    image: require('../../assets/images/m-bg-1.jpg'),
+  }, {
+    name: 'Fozly',
+    price: '$1920',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/bg-2.jpg'),
+  }, {
+    name: 'Gopal Vog',
+    price: '$1920',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/m-bg-1.jpg'),
+  }, {
+    name: 'Bari four',
+    price: '$1920',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/m-bg-2.jpg'),
+  }];
